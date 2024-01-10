c29aece5f6869788cef7b595eb798ce683f24d6d
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 09:59:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Get_user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi, "+name);
+            Console.WriteLine("how old are you?");
+           int age = int.Parse(Console.ReadLine());
+            Console.WriteLine(name+" your age is " + age);
+
+
+
+        }
+    }
+}
